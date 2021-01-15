c8c9f29568cecfb04c2d1dd7f356c7e2bd6edf57
Nathan Schrader <nathanschrader@icloud.com>
2021-01-15 13:33:34 -0800

    add AddTreat routes

--- PierresInventory/Controllers/FlavorsController.cs
+++ PierresInventory/Controllers/FlavorsController.cs
@@ -99,7 +99,22 @@ namespace PierresInventory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
+    public ActionResult AddTreat(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      return View(thisFlavor);
+    }
+    [HttpPost]
+    public ActionResult AddTreat(Flavor flavor, int TreatId)
+    {
+      if (TreatId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }
