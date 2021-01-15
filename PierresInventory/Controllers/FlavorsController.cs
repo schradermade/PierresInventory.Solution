e78abd2debf9a0a19045d12660fa797c33787360
Nathan Schrader <nathanschrader@icloud.com>
2021-01-15 13:06:04 -0800

    add Index route

--- PierresInventory/Controllers/FlavorsController.cs
+++ PierresInventory/Controllers/FlavorsController.cs
@@ -4,11 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierresInventory.Controllers
 {
   public class FlavorsController : Controller
   {
+    private readonly PierresInventoryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    
+    public FlavorsController(UserManager<ApplicationUser> userManager, PierresInventoryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+        var userId = this._userManager.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var currentUser = await _userManager.FindByIdAsync(userId);
+        var userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+        return View(userFlavors);
+    }
+
+   
+  
 
   }
 }
