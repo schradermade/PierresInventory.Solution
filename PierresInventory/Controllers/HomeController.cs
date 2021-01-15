4236ff2b4bf8cc4aae98711dbbeeb4cb4a7f1346
Nathan Schrader <nathanschrader@icloud.com>
2021-01-15 15:39:49 -0800

    add _db, HomeController method

--- PierresInventory/Controllers/HomeController.cs
+++ PierresInventory/Controllers/HomeController.cs
@@ -9,10 +9,24 @@ namespace PierresInventory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresInventoryContext _db;
+    public HomeController(PierresInventoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      var treatList = _db.Treats.ToList();
+      var flavorList = _db.Flavors.ToList();
+      Dictionary<string, object> myDictionary = new Dictionary<string, object> ();
+      myDictionary.Add("Treat", treatList);
+      myDictionary.Add("Flavor", flavorList);
+
+      ViewBag.manyFlavors = _db.Flavors;
+      ViewBag.manyTreats = _db.Treats;
+      return View(myDictionary);
     }
   }
 }
