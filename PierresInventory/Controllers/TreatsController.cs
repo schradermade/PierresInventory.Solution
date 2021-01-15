86b7ef98cff1d6cfde4112f72ad117d73a8e0d7a
Nathan Schrader <nathanschrader@icloud.com>
2021-01-15 12:49:09 -0800

    add Index, SearchTreat, Create routes

--- PierresInventory/Controllers/TreatsController.cs
+++ PierresInventory/Controllers/TreatsController.cs
@@ -9,6 +9,39 @@ namespace PierresInventory.Controllers
 {
   public class TreatsController : Controller
   {
+    private readonly PierresInventoryContext _db;
+    public TreatsController(PierresInventoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
+    }
+    public ActionResult SearchTreat(string name)
+    {
+          var thisTreat = _db.Treats
+          .Include(treat => treat.JoinEntries)
+          .ThenInclude(join => join.Flavor)
+          .FirstOrDefault(treat => treat.TreatName == name);
+      return View(thisTreat);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create (Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+
+  
 
   }
 }
